adf182dfad0987d314af6c0226856d48ab00aac0
Kaushlendra Pathe <kpathe972@gmail.com>
2026-01-02 17:49:27 +0530

    Implemented file organizer as a commandline tool using fs node-path and fs-extra modules

---
+++ organize.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const fsmove = require("fs-extra");
+const path = require("node:path");
+
+const filepath = process.argv[2];
+
+// reading folder
+const folder = fs.readdirSync(filepath);
+
+// categorising files according to extension names
+
+const img = [];
+const vid = [];
+const doc = [];
+
+for (const file of folder) {
+  switch (true) {
+    case path.extname(`${filepath}\\${file}`) === ".jpg":
+      img.push(file);
+      break;
+    case path.extname(`${filepath}\\${file}`) === ".mp4":
+      vid.push(file);
+      break;
+    case path.extname(`${filepath}\\${file}`) === ".pdf":
+      doc.push(file);
+      break;
+
+    default:
+      break;
+  }
+}
+
+fs.mkdirSync(`${filepath}\\Images`);
+fs.mkdirSync(`${filepath}\\Videos`);
+fs.mkdirSync(`${filepath}\\Docs`);
+
+for (const image of img) {
+  fsmove.moveSync(`${filepath}\\${image}`, `${filepath}\\Images\\${image}`);
+}
+for (const video of vid) {
+  fsmove.moveSync(`${filepath}\\${video}`, `${filepath}\\Videos\\${video}`);
+}
+for (const document of doc) {
+  fsmove.moveSync(`${filepath}\\${document}`, `${filepath}\\Docs\\${document}`);
+}
