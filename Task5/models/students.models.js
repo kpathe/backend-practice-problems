a8c7c441fa06daa1cad20c3bf742699f60701fff
Kaushlendra Pathe <kpathe972@gmail.com>
2026-01-07 13:50:30 +0530

    Created schema for students

---
+++ Task5/models/students.models.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const studentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    rollnum: {
+      type: String,
+      required: true,
+    },
+    department: {
+      type: String,
+      require: true,
+    },
+    subjects: [{ type: String }],
+  },
+  { timestamps: true }
+);
+
+const Student = mongoose.model("student", studentSchema);
+
+module.exports = Student;
