c16cf99abb9503407811e4f7a380590ebf30d56b
Kaushlendra Pathe <kpathe972@gmail.com>
2026-01-21 14:48:46 +0530

    basic boilerplate done

---
+++ Task8/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at PORT : ${PORT}`);
+});
