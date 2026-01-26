974a6e59cd17af1c50a628689e5f01f72c055aae
Kaushlendra Pathe <kpathe972@gmail.com>
2026-01-26 17:55:28 +0530

    task9 clustering

---
+++ Task9/server.js
@@ -0,0 +1,23 @@
+const cluster = require("node:cluster");
+const os = require("os");
+const totalCPUs = os.cpus().length;
+const app = require("./app");
+// console.log(totalCPUs);
+
+console.log(cluster.isPrimary);
+
+if (cluster.isPrimary) {
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
+} else {
+  const PORT = 8000;
+  app.get("/", (req, res) => {
+    console.log(`Worker ${process.pid} handled request`); 
+    return res.json({ message: `Hello from Express Server ${process.pid}` });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server started at PORT : ${PORT}`);
+  });
+}
