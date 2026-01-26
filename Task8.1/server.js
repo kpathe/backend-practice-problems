c52c78a6afc8a41187dd74787d4fe57b2155d68b
Kaushlendra Pathe <kpathe972@gmail.com>
2026-01-26 17:28:39 +0530

    task 10 completed

--- Task8.1/server.js
+++ Task8.1/server.js
@@ -2,16 +2,43 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 
-const fileStats = fs.statSync("./public/video.mp4");
-const fileSize = fileStats.size;
-
 app.get("/video", (req, res) => {
-  const stream = fs.createReadStream("./public/video.mp4");
-  res.writeHead(200, {
-    "Content-Length": fileSize,
-    "Content-Type": "video/mp4",
-  });
-  stream.pipe(res);
+  const videoSize = fs.statSync("./public/video.mp4").size;
+
+  const range = req.headers.range;
+
+  if (!range) {
+    res.writeHead(200, {
+      "content-length": videoSize,
+      "Content-Type": "video/mp4",
+    });
+
+    fs.createReadStream("./public/video.mp4").pipe(res);
+  } else {
+    const chunk_size = 10 ** 6;
+    const start = Number(range.replace(/\D/g, ""));
+
+    const end = Math.min(start + chunk_size, videoSize - 1);
+
+    const contentLength = end - start + 1;
+
+    console.log(start, end, contentLength,videoSize,range);
+
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "video/mp4",
+    };
+
+    res.writeHead(206, headers);
+
+    const videoStream = fs.createReadStream("./public/video.mp4", {
+      start,
+      end,
+    });
+    videoStream.pipe(res);
+  }
 });
 
 app.listen(4000, () => {
